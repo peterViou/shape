d4e9e32c82749d7044162dda7ede69f80760caca
Duchastenier, Peter <PDuchastenier@nds.com>
2017-06-15 16:22:40 +0200

    logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {MasonryOptions} from "angular2-masonry";
+import {Component, ViewChild} from '@angular/core';
+import {MasonryOptions, AngularMasonry} from "angular2-masonry";
 
 @Component({
   selector: 'app-root',
@@ -7,29 +7,93 @@ import {MasonryOptions} from "angular2-masonry";
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent   {
-  articles = [
-    {title: '1', artist:'Boualam Allouche', type:"video"},
-    {title: '2', artist:'Stéphanie de Saint Marc', type:"video"},
-    {title: '3', artist:'Peter Duchastenier', type:"video"},
-    {title: '4', artist:'Stéphanie de Saint Marc', type:"video"},
-    {title: '5', artist:'Stéphanie de Saint Marc', type:"video"},
-    {title: '6', artist:'Stéphanie de Saint Marc', type:"video"},
-    {title: '7', artist:'Marcia Cortes Ortega'},
-    {title: '8', artist:'Marcia Cortes Ortega'}
-  ];
+export class AppComponent {
+
+  // public cacheInitLength = 10;
+  private cacheMoreLength = 10;
+  public cacheLength = 10;
+  private latestMasonry: AngularMasonry //= my;
 
   public myOptions: MasonryOptions = {
-    transitionDuration: '0.4s',
+    transitionDuration: '.00000001s',
     resize: true
   };
 
-  onScroll () {
+  @ViewChild('myMasonry') private monMacon;
 
-    for(var i = 1; i <= 8; i++) {
+  articles = [
+    {title: '1', artist: 'Boualam Allouche', type: "video"},
+    {title: '2', artist: 'Stéphanie de Saint Marc', type: "video"},
+    {title: '3', artist: 'Peter Duchastenier', type: "video"},
+    {title: '4', artist: 'Stéphanie de Saint Marc', type: "video"},
+    {title: '5', artist: 'Stéphanie de Saint Marc', type: "video"},
+    {title: '6', artist: 'Stéphanie de Saint Marc', type: "video"},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '7', artist: 'Marcia Cortes Ortega'},
+    {title: '8', artist: 'Marcia Cortes Ortega'}
+  ];
+
+
+
+  cacheInit() {
+
+    // myMasonry.add()
+    // this.
+  }
+
+  refreshed() {
+    console.log("refreshed !")
+    this.monMacon.layout();
+  }
+
+  onScroll() {
+
+    for (var i = 1; i <= this.cacheMoreLength; i++) {
       this.articles.push(this.articles[i]);
+      this.monMacon.layout();
     }
+
+    // for (var i = 1; i <= this.cacheMoreLength; i++) {
+    //   this.monMacon.add();
+    // }
+
+
     console.log(this.articles.length + ' photo loaded');
+    // $route.reload();
+    // $window.location.reload();
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,12 @@ import {HeaderComponent} from './component/header/header.component';
 import {InfiniteScrollModule} from "angular2-infinite-scroll";
 // import {FlickityModule} from "ngx-flickity";
 // import {VirtualScrollModule} from "angular2-virtual-scroll";
-import { ListItemComponent } from './component/list-item/list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleComponent,
     HeaderComponent,
-    ListItemComponent
   ],
   imports: [
     BrowserModule,
