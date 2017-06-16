ec9083532601f6460f1a3d5515e6b76772c56c91
Duchastenier, Peter <PDuchastenier@nds.com>
2017-06-16 16:34:08 +0200

    header : nav, instagram

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,42 +8,49 @@ import {MasonryOptions} from "angular2-masonry";
 })
 
 export class AppComponent implements OnInit {
+
+
+  private articles = [];
   private cacheInitLength = 10;
   private cacheMoreLength = 10;
+
+  // Instanciation of masonry's component "monMacon" declared in the template into the controller
+  @ViewChild('myMasonry') private monMacon;
   public myOptions: MasonryOptions = {
     transitionDuration: '0.8s',
     resize: true,
-    hiddenStyle: { opacity: 0 }
+    hiddenStyle: { opacity: 0 },
+    fitWidth: true
   };
-
-  @ViewChild('myMasonry') private monMacon;
-
-  articles = [];
 
   constructor() {
   }
 
-  ngOnInit() {
+  ngOnInit():void {
     for (var i = 1; i <= this.cacheInitLength; i++) {
       var width = this.randomInt(150, 400);
       var height = this.randomInt(150, 250);
-      // var toto = 'http://via.placeholder.com/' + width + 'x' + height + '/cccccc';
-      var toto = 'https://lorempixel.com/' + width + '/' + height + '/fashion';
+      var toto = 'http://via.placeholder.com/' + width + 'x' + height + '/cccccc';
+      // var toto = 'https://lorempixel.com/' + width + '/' + height + '/fashion';
       var monArticle = {title: i, artist:"Peter", src: toto}
       this.articles.push(monArticle);
     }
   }
 
-  private randomInt(min, max) {
+  private randomInt(min, max):number {
     return Math.floor(Math.random() * max + min);
   }
 
-  refreshed() {
+  /**
+   * Force the redraw of Masonry
+   */
+  refreshed():void {
     console.log("refreshed !")
     this.monMacon.layout();
+
   }
 
-  onScroll() {
+  onScroll():void {
     for (var i = 1; i <= this.cacheMoreLength; i++) {
       this.articles.push(this.articles[i]);
     }
