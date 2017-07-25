54ee755c1603e21eac91dec8aeb70af784700792
Duchastenier, Peter <PDuchastenier@nds.com>
2017-07-25 18:28:09 +0200

    activation de la lightBox !!!

--- src/app/component/asset-list/asset-list.component.ts
+++ src/app/component/asset-list/asset-list.component.ts
@@ -4,6 +4,12 @@ import {DataService} from "../latest-list/data.service";
 import {Lightbox} from 'angular2-lightbox';
 import {IAsset} from "../latest-list/iasset";
 
+export interface IAlbum {
+  src: string;
+  caption?: string;
+  thumb: string;
+}
+
 @Component({
   selector: 'app-asset-list',
   templateUrl: './asset-list.component.html',
@@ -12,12 +18,14 @@ import {IAsset} from "../latest-list/iasset";
 export class SerieViewerComponent implements OnInit {
 
   public displayedAssets: IAsset[];
+  public displayedAlbum: IAlbum[];
   public myOptions: MasonryOptions = {
     transitionDuration: '0s',
     resize: true,
     hiddenStyle: {opacity: 0},
     fitWidth: true,
   };
+
 
   @ViewChild('myMasonry2') private _masonryInstance; // _masonryInstance : Variable linked to masonry's component instantiated in the template
 
@@ -28,13 +36,25 @@ export class SerieViewerComponent implements OnInit {
   ngOnInit() {
     const serieToDisplay = this._dataService.serieToDisplay;
     this.displayedAssets = serieToDisplay.assets;
+
+    this.displayedAlbum = this.displayedAssets.map(album => {
+      return {
+        // assets: [album],
+        src : album.thumbnail,
+        caption: album.thumbnail,
+        thumb: album.thumbnail
+      }
+    })
+
+    console.log("displayedAlbum : " + this.displayedAlbum[0].src);
+
   }
 
 
   public onAssetClick(assetID: number): void {
     console.log("assetID : " + assetID);
 
-    // this._lightbox.open(this.bigImages, assetID);
+    this._lightbox.open(this.displayedAlbum, assetID);
     // TODO
   }
 
