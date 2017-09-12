f3b29eafa4dcddfbd66c58db42744146d2984c17
Duchastenier, Peter <PDuchastenier@nds.com>
2017-09-12 11:15:43 +0200

    responsivness header>logo, header.height, serie-info.y, insta.x

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-_isInit:boolean;
+_isInit:boolean = false;
   constructor() { }
 
   ngOnInit():boolean {
